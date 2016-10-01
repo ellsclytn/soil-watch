d4cf2965a3c5d764da09175f90fc3788c3124825
Ellis Clayton <ellis@ellisclayton.info>
2016-10-01 12:03:12 +1000

    Initial commit

---
+++ src/soil-watch.ino
@@ -0,0 +1,76 @@
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+#include <PubSubClient.h>
+#include "ThingSpeak.h"
+#include "env.h"
+
+Adafruit_ADS1115 ads;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char readingBuffer[7];
+
+void setupWifi() {
+  WiFi.disconnect(true);
+  delay(500);
+
+  Serial.print("Connecting to: ");
+  Serial.println(SSID);
+
+  WiFi.begin(SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.print(WiFi.localIP());
+  Serial.print(" with MAC: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection... ");
+
+    if (client.connect(WiFi.macAddress().c_str(), USER, MQ_PASS)) {
+      Serial.println("connected.");
+      client.subscribe("pr");
+    } else {
+      Serial.print("failed, rc: ");
+      Serial.print(client.state());
+      Serial.println(" will retry in 5 seconds.");
+
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  ads.begin();
+  setupWifi();
+  client.setServer(SERVER, PORT);
+  ThingSpeak.begin(espClient);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  int16_t reading = ads.readADC_SingleEnded(0);
+  String readingString = String(reading);
+  readingString.toCharArray(readingBuffer, readingString.length() + 1);
+  client.publish("soilMoisture", readingBuffer);
+  ThingSpeak.writeField(TS_CHANNEL_ID, 1, readingBuffer, TS_API_KEY);
+  Serial.print("Sent reading: ");
+  Serial.println(readingBuffer);
+
+  delay(20000);
+}
